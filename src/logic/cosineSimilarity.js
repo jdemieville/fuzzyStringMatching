24593a44a1f538e8bbe41218722a999725cad932
jdemieville <demievillecodes@gmail.com>
2019-11-10 14:32:04 -0600

    cosine similarity logic and display added

---
+++ src/logic/cosineSimilarity.js
@@ -0,0 +1,55 @@
+import { all } from "q";
+
+export default function cosineSimilarity(str1, str2){
+    // map used to enforce unique word keys
+    let wordMap = new Map();
+    let allWords = [];
+    let words1 = str1.split(' ');
+    let words2 = str2.split(' ');
+    let wordFreq1 = [];
+    let wordFreq2 = [];
+    let dotProduct = 0;
+    let eucNorm1 = 0;
+    let eucNormFinal1;
+    let eucNorm2 = 0;
+    let eucNormFinal2;
+    let cosSim;
+    // count number of times all words appear in both texts
+    // fill array with words from strings
+    words1.forEach(word => {
+        if(!allWords.includes(word)){
+            allWords.push(word)
+        }
+    });
+    words2.forEach(word => {
+        if(!allWords.includes(word)){
+            allWords.push(word)
+        }
+    });
+    for(let index = 0; index < allWords.length; index++){
+        if(words1.includes(allWords[index])){
+            wordFreq1[index] !== undefined ? wordFreq1[index] += 1 : wordFreq1[index] = 1;
+        } else {
+            wordFreq1[index] = 0;
+        }
+        if(words2.includes(allWords[index])){
+            wordFreq2[index] !== undefined ? wordFreq2[index] += 1 : wordFreq2[index] = 1;
+        } else {
+            wordFreq2[index] = 0;
+        }
+    }
+    // calculate the dot product between the 2 frequency vectors as well as the Euclidean norm of the 2 vectors
+    for(let vectorIndex = 0; vectorIndex < wordFreq1.length; vectorIndex++){
+        dotProduct += wordFreq1[vectorIndex]*wordFreq2[vectorIndex];
+        eucNorm1 += Math.pow(wordFreq1[vectorIndex],2);
+        eucNorm2 += Math.pow(wordFreq2[vectorIndex],2);
+    }
+    // square products of vector squares for final Euclidean norm
+    eucNormFinal1 = Math.sqrt(eucNorm1, 2);
+    eucNormFinal2 = Math.sqrt(eucNorm2, 2);
+
+    // calculate cosine between the vectors
+    cosSim = dotProduct/(eucNormFinal1*eucNormFinal2);
+
+    return cosSim;
+}
